bd6537195c8cf6d0fbf7082c8a1739157882b3ff
Eduardo Custodio <eduardo.gimenes.custodio@gmail.com>
2020-12-23 10:00:55 -0400

    Tela de projetos responsiva

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -11,17 +11,21 @@ React.Component {
                 <div className="title-box">
                     <h1 className="title">{this.props.title}</h1>
                 </div>
-                <div className="description-box">
-                    <h2 className="logo">{this.props.text_logo}</h2>
-                    <img src={this.props.src} alt={this.props.alt} ></img>
-                    
-                    <p className="text">{this.props.text}</p>
-                    <p className="text">{this.props.technologies}</p>
-                    <div className="buttons">
-                        <Button text="veja no GitHub" href={this.props.github} target="_blank" />
-                        <Button text="veja online" href={this.props.link} target="_blank" />
+                <section className="description-box">
+                    <div>
+                        <h2 className="logo">{this.props.text_logo}</h2>
+                        <img src={this.props.src} alt={this.props.alt} ></img>
                     </div>
-                </div>
+
+                    <div>
+                        <p className="text">{this.props.text}</p>
+                        <p className="text">{this.props.technologies}</p>
+                        <div className="buttons">
+                            <Button text="veja no GitHub" href={this.props.github} target="_blank" />
+                            <Button text="veja online" href={this.props.link} target="_blank" />
+                        </div>                        
+                    </div>
+                </section>
             </div>
         );
     }
