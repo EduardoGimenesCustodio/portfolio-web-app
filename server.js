7a14847652f618b99765f45f07b2a6833ba15fe6
Eduardo Custodio <eduardo.gimenes.custodio@gmail.com>
2021-01-01 11:11:22 -0400

    Adição do arquivo server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const configs = {
+    caminho: "public", //Aqui será definido a pasta de saída onde contém o index.html e os outros arquivos.
+    port: process.env.PORT || 3000
+}
+
+app.use(express.static(configs.caminho)); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
+
+app.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
+    res.sendFile(path.join(__dirname, configs.caminho, "index.html"));
+});
+
+app.listen(configs.port, () => {
+    console.log('Escutando na ${configs.port}!');
+});
