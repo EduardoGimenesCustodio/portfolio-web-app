f944293f21eb5a72e444516d9d2c583f8702417b
Eduardo Henrique Gimenes Custodio <eduardo.gimenes.custodio@gmail.com>
2023-08-21 12:40:06 -0400

    feat: update descriptions

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,14 +5,19 @@ import "./styles.css";
 import ButtonLink from "./../ButtonLink";
 
 const About = () => (
-    <div className="about">
-        <h1 className="title">Sobre mim</h1>
-        <p className="text">Desenvolvedor web, técnico em Informática para Internet pelo IFMS e estudante de Sistemas de Informação na UFGD.</p>
-        
-        <div className="buttons">
-            <ButtonLink text="ver projetos" to="/projects" />
-        </div>
+  <div className="about">
+    <h1 className="title">Sobre mim</h1>
+    <p className="text">
+      Desenvolvedor web, técnico em Informática para Internet pelo IFMS e
+      estudante de Sistemas de Informação na UFGD. Possuo experiência com
+      programação back-end e web design. Atualmente, meu foco de atuação
+      profissional é a área de desenvolvimento full-stack.
+    </p>
+
+    <div className="buttons">
+      <ButtonLink text="ver projetos" to="/projects" />
     </div>
+  </div>
 );
 
 export default About;
