477c3b3fb330a77520b7f552eb109d5a6a038761
Eduardo Henrique Gimenes Custodio <eduardo.gimenes.custodio@gmail.com>
2023-08-22 14:55:01 -0400

    refactor: make links optional

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,30 +3,39 @@ import React from "react";
 import "./styles.css";
 import Button from "./../Button";
 
-export default class Project extends 
-React.Component {
-    render() {
-        return (
-            <div className={this.props.color}>
-                <div className="title-box">
-                    <h1 className="title">{this.props.title}</h1>
-                </div>
-                <section className="description-box">
-                    <div>
-                        <h2 className="logo">{this.props.text_logo}</h2>
-                        <img src={this.props.src} alt={this.props.alt} ></img>
-                    </div>
+export default class Project extends React.Component {
+  render() {
+    return (
+      <div className={this.props.color}>
+        <div className="title-box">
+          <h1 className="title">{this.props.title}</h1>
+        </div>
+        <section className="description-box">
+          <div>
+            <h2 className="logo">{this.props.text_logo}</h2>
+            <img src={this.props.src} alt={this.props.alt}></img>
+          </div>
 
-                    <div>
-                        <p className="text">{this.props.text}</p>
-                        <p className="text">{this.props.technologies}</p>
-                        <div className="buttons">
-                            <Button text="veja no GitHub" href={this.props.github} target="_blank" />
-                            <Button text="veja online" href={this.props.link} target="_blank" />
-                        </div>                        
-                    </div>
-                </section>
+          <div>
+            <p className="text">{this.props.text}</p>
+            <p className="text">{this.props.technologies}</p>
+            <div className="buttons">
+              <Button
+                text="veja no GitHub"
+                href={this.props.github}
+                target="_blank"
+              />
+              {this.props.link !== undefined && (
+                <Button
+                  text="veja online"
+                  href={this.props.link}
+                  target="_blank"
+                />
+              )}
             </div>
-        );
-    }
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
